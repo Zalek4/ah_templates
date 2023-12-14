3d6d5713a96dd104452979876ac5511ebdc69932
Zalek4 <alexhallenbeck1@gmail.com>
2023-12-13 21:11:19 -0800

    PYTHON - Added a simple template for handling user input with the program console.

---
+++ python/ah_python_simple_console_01.py
@@ -0,0 +1,12 @@
+# Initial prompt.
+print("Hello! Please type something below and then hit the 'Enter' key.")
+
+# Wait for user input, and assign the input to a variable.
+user_input = input()
+
+# Print the user input back
+print(f"You typed : '{user_input}'")
+
+# Prompt the user to hit any key to kill the program.
+print("Hit 'Enter' to exit...")
+input()
